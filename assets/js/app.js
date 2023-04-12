bee02ae02691b4e22e829984eaadda41d87d34aa
JosephGlz <joseph220199@gmail.com>
2023-04-12 15:55:04 -0600

    Generate Truthtable

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 var btnStepOne = document.getElementById('btnStepOne');
+var btnStepTwo = document.getElementById('btnStepTwo');
 var contFirst = document.getElementById('ContFirstAnim');
 var contentInputs = document.getElementById('contentInputs');
 var contTruthTable = document.getElementById('ContTruthTable');
@@ -7,56 +8,75 @@ var contentEcuation = document.getElementById('contentEcuation');
 numVariables.addEventListener('change', function () {
   contentInputs.classList.add('slide-left');
   if (document.getElementById('numVariables').value > 0 && document.getElementById('numVariables').value <= 6) {
-    const truthTable = generateTruthTable(document.getElementById('numVariables').value);
-    contTruthTableAnim();
-
-    displayTruthTable(truthTable);
-  } else {
-    alert('Por favor seleccione un número de variables válido');
-  }
-});
-
-btnStepOne.addEventListener('click', function () {
-  if (document.getElementById('numVariables').value > 0 && document.getElementById('numVariables').value <= 6) {
+    
     document.getElementById('equation_input').classList.remove('hidden');
     document.getElementById('equation_input').classList.add('flex');
     contNumBarAnim();
     contEcuationAnim();
     contentEcuation.setAttribute('style', 'transform: : tranlateX(50rem);');
-    // const truthTable = generateTruthTable(document.getElementById('numVariables').value);
-
-    // displayTruthTable(truthTable);
   } else {
     alert('Por favor seleccione un número de variables válido');
   }
 });
 
-function generateTruthTable(numVariables) {
-  const numCombinations = Math.pow(2, numVariables);
+btnStepTwo.addEventListener('click', function () {
+  if(document.getElementById('equation').value != '') {
+    var salidas = fixEquation(document.getElementById('equation').value)
+
+    const truthTable = generateTruthTable(document.getElementById('numVariables').value, salidas);
+    contTruthTableAnim();
+
+    displayTruthTable(truthTable);
+  }
+})
+
+function fixEquation(equation) {
+  var equationFixed = equation.slice(1, -1)
+  var finalEquation = equation.split(',')
+  return finalEquation
+}
+
+function generateTruthTable(numVariables, salidas) {
   const truthTable = [];
+  const numRows = Math.pow(2, numVariables);
 
-  for (let i = 0; i < numCombinations; i++) {
+  for (let i = 0; i < numRows; i++) {
     const row = [];
-
     for (let j = 0; j < numVariables; j++) {
-      const variableValue = (i & Math.pow(2, numVariables - j - 1)) ? 1 : 0;
-      row.push(variableValue);
+      row.push(Math.floor(i / Math.pow(2, j)) % 2);
     }
-
     truthTable.push(row);
   }
 
+  for(var i = 0; i < truthTable.length; i++){
+    for(var j = 0; j < salidas.length; j++) {
+        if(salidas[j] == i) {
+          truthTable[i][4] = 1
+        }else if(truthTable[i][4] != 1) {
+          truthTable[i][4] = 0
+        }
+    }
+  }
   return truthTable;
+}
+
+const variablesLetter = {
+  0: 'A',
+  1: 'B',
+  2: 'C',
+  3: 'D',
+  4: 'E',
+  5: 'F',
+  6: 'G'
 }
 
 function displayTruthTable(truthTable) {
   const tableElement = document.createElement('table');
-
   // Crear encabezado
   const headerRow = document.createElement('tr');
   for (let i = 0; i < truthTable[0].length; i++) {
     const headerCell = document.createElement('th');
-    headerCell.textContent = `Var${i + 1}`;
+    headerCell.textContent = `${variablesLetter[i]}`;
     headerRow.appendChild(headerCell);
   }
   tableElement.appendChild(headerRow);
